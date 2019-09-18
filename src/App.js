c2128f320d151bccd84ac4e92a8c38a939bdd6b6
Chris Martin <chris@sensedev.com>
2019-09-18 15:03:20 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+import Game from './Game';
+
+/** Simple app that just shows the LightsOut game. */
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+          <Game
+          />
+        </div>
+    );
+  }
+}
+
+export default App;
